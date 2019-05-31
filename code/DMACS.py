90f23f99b49931c67cdf4690855e4705e87b922a
Nicolo Savioli <nsavioli@mrc-10751.isd.csc.mrc.ac.uk>
2019-05-31 10:22:47 +0100

    First commit

--- code/DMACS.py
+++ code/DMACS.py
@@ -28,7 +28,7 @@ tf.app.flags.DEFINE_string('test_dir',      '/cardiac/UKBB_40616/test_ukbb',
                                             'Path to the test set directory, under which images are organised in '
                                             'subdirectories for each subject.')
 tf.app.flags.DEFINE_string('model_path',    '/model/FCN_sa', 'Path to the saved trained model.')
-tf.app.flags.DEFINE_string('atlas_dir',     '/refs', 'Path to the atlas.')
+tf.app.flags.DEFINE_string('atlas_dir',     '/cardiac/patchmatchSegmentation/3datlas2', 'Path to the atlas.')
 tf.app.flags.DEFINE_string('param_dir',     '/par', 'Path to the registration parameters.')
 tf.app.flags.DEFINE_string('template_dir',  '/vtks/1', 'Path to the template.')
 tf.app.flags.DEFINE_string('template_PH',   '/vtks/2', 'Path to the template.')
