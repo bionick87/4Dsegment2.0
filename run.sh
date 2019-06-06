93c8927737cb34a5517468e8603e4d9a6048540b
Nicolo Savioli <nsavioli@huai.doc.ic.ac.uk>
2019-06-06 18:53:32 +0100

    First commit

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run -it --rm -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/data/:/data -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/code/:/code -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/model/:/model -v /home/nsavioli@isd.csc.mrc.ac.uk/cardiac/:/cardiac  -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2/par_ukbb/:/par_ukbb  jinmingduan/segmentationmeshmotion /bin/bash
+docker run -it --rm -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/data/:/data -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/code/:/code -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/model/:/model -v /home/nsavioli@isd.csc.mrc.ac.uk/cardiac/:/cardiac  -v /home/nsavioli@isd.csc.mrc.ac.uk/4Dsegment2.0/par_ukbb/:/par_ukbb  jinmingduan/segmentationmeshmotion /bin/bash
 
