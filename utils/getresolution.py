eed90ca4d34e2ad52da865e5499fdaf6c36d9ea8
Nicolo Savioli <nsavioli@huai.doc.ic.ac.uk>
2019-06-19 18:58:59 +0100

    First commit

--- utils/getresolution.py
+++ utils/getresolution.py
@@ -23,34 +23,39 @@ def scan_high_paths(high_path,target):
         type(target)
         if target in path.split("_"):
            get_path = path
-           print("\n ... SONOO QUIIIIIIIIIIIIIIIIIII "+ str(get_path) +"\n")
            break
-    print("sONO QUIII 2 "+ str(get_path))
     return  get_path
 
 
 def getnii(low_path,high_path,\
            new_path,low_files,\
            high_files,seg_files):
-       
+    list_not = []   
     for patient in os.listdir(low_path):
         high_new_path,\
         low_new_path,\
         high_seg         = get_new_folders(new_path,patient)
         print("\n ..." + patient)
         # low resolution files 
-        for fr in low_files:
-            copyfile(os.path.join(low_path,patient,fr), os.path.join(low_new_path,patient))
         old_high_path = scan_high_paths(high_path,patient)
-        for fr in high_files:
-            copyfile(os.path.join(high_path,old_high_path,fr), os.path.join(low_new_path,patient))
-        for fr in seg_files:
-            copyfile(os.path.join(high_path,old_high_path,fr), os.path.join(high_seg,patient))
+        if old_high_path !="":
+            for fr in low_files:
+                copyfile(os.path.join(low_path,patient,fr), os.path.join(low_new_path,patient))
+            
+            for fr in high_files:
+                copyfile(os.path.join(high_path,old_high_path,fr), os.path.join(low_new_path,patient))
+            for fr in seg_files:
+                copyfile(os.path.join(high_path,old_high_path,fr), os.path.join(high_seg,patient))
+        else:
+            list_not.append(patient)
+            continue
+    return list_not
 
 def main(low_path,high_path,\
          new_path,low_files,\
          high_files,seg_files):
-    getnii(low_path,high_path,new_path,low_files,high_files,seg_files)
+    list_not = getnii(low_path,high_path,new_path,low_files,high_files,seg_files)
+    print(list_not)
 
 
 if __name__ == "__main__":
